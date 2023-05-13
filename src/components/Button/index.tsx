9aea2a453c38272c93feea8cdf392315a42ef7a8
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-13 16:46:19 -0300

    feat: finalização layout tela de sign in

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import { TouchableOpacityProps } from 'react-native';
+
+import { Container, Loading, Title } from './styles';
+
+type ButtonProps = TouchableOpacityProps & {
+  title: string;
+  isLoading?: boolean;
+};
+
+export function Button({ title, isLoading = false, ...rest }: ButtonProps) {
+  return (
+    <Container activeOpacity={0.7} disabled={isLoading} {...rest}>
+      {isLoading ? <Loading /> : <Title>{title}</Title>}
+    </Container>
+  );
+}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,14 @@
-import { Text, View } from 'react-native';
+import backgroundImg from '@assets/background.png';
+import { Button } from '@components/Button';
 
-import { styles } from './styles';
+import { Container, Slogan, Title } from './styles';
 
 export default function SignIn() {
   return (
-    <View style={styles.container}>
-      <Text>Ignite Fleet</Text>
-    </View>
+    <Container source={backgroundImg}>
+      <Title>Ignite Fleet</Title>
+      <Slogan>Gestão de uso de veículos</Slogan>
+      <Button title="Entrar com o Google" />
+    </Container>
   );
 }
