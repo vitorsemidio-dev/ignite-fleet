8da1df600841f0a9b27291c510dfb3b0e97445ac
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-13 15:59:34 -0300

    feat: criação projeto e configurado path alias

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,29 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@config': './src/config',
+            '@constants': './src/constants',
+            '@context': './src/context',
+            '@hooks': './src/hooks',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@storage': './src/storage',
+            '@styles': './src/styles',
+            '@theme': './src/theme',
+            '@utils': './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export default function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Text>Ignite Fleet</Text>
+    </View>
+  );
+}
