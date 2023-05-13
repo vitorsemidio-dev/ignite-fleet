c9e219fa36755afadc01ef65b6ab6b057834e1f3
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-13 16:17:05 -0300

    feat: instalação styled components + tipagem

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import { ThemeProvider } from 'styled-components';
 
 import SignIn from '@screens/SignIn';
+import theme from '@theme/index';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <StatusBar style="auto" />
       <SignIn />
-    </>
+    </ThemeProvider>
   );
 }
