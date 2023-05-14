1d1c3812a4f0ffb82b9328f304b5434a98ffd35d
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-14 00:39:55 -0300

    feat: instalação realm db

--- App.tsx
+++ App.tsx
@@ -3,10 +3,13 @@ import {
   Roboto_700Bold,
   useFonts,
 } from '@expo-google-fonts/roboto';
+import { AppProvider, UserProvider } from '@realm/react';
 import { StatusBar } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 
 import { Loading } from '@components/Loading';
+import { REALM_APP_ID } from '@env';
+import { Home } from '@screens/Home';
 import SignIn from '@screens/SignIn';
 import theme from '@theme/index';
 
@@ -20,13 +23,17 @@ export default function App() {
     return <Loading />;
   }
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
