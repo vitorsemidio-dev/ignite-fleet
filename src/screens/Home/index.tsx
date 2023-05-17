d376a807f2c256fe93427471473ab25b69812eb0
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-17 01:48:41 -0300

    feat: busca veículo em uso

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,22 +1,42 @@
-import React from 'react';
 import { useNavigation } from '@react-navigation/native';
+import React, { useEffect, useState } from 'react';
 
 import { CarStatus } from '@components/CarStatus';
 import { HomeHeader } from '@components/HomeHeader';
+import { useQuery } from '@libs/realm';
+import { Historic } from '@libs/realm/schemas/Historic';
+
 import { Container, Content } from './styles';
 
 export function Home() {
+  const [vehicleInuse, setVehicleInUse] = useState<Historic | null>(null);
+
   const { navigate } = useNavigation();
+  const historic = useQuery(Historic);
 
   function handleRegisterMovement() {
     navigate('departure');
   }
 
+  function fetchVehicle() {
+    const vehicles = historic.filtered("status = 'departure'");
+    if (vehicles.length > 0) {
+      setVehicleInUse(vehicles[0]);
+    }
+  }
+
+  useEffect(() => {
+    fetchVehicle();
+  }, []);
+
   return (
     <Container>
       <HomeHeader />
       <Content>
-        <CarStatus licensePlate="XXX-1234" onPress={handleRegisterMovement} />
+        <CarStatus
+          licensePlate={vehicleInuse?.license_plate}
+          onPress={handleRegisterMovement}
+        />
       </Content>
     </Container>
   );
