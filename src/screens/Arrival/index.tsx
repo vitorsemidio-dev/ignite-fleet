c3e655327bc2b8ea2429c9ac0e7d896f68cf95f2
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-17 02:06:31 -0300

    feat: busca detalhes do veículo na chegada

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,4 +1,12 @@
 import { useRoute } from '@react-navigation/native';
+import { X } from 'phosphor-react-native';
+import { BSON } from 'realm';
+
+import { Button } from '@components/Button';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { Header } from '@components/Header';
+import { useObject } from '@libs/realm';
+import { Historic } from '@libs/realm/schemas/Historic';
 
 import {
   Container,
@@ -8,10 +16,6 @@ import {
   Label,
   LicensePlate,
 } from './styles';
-import { Header } from '@components/Header';
-import { Button } from '@components/Button';
-import { ButtonIcon } from '@components/ButtonIcon';
-import { X } from 'phosphor-react-native';
 
 type RouteParamProps = {
   id: string;
@@ -22,25 +26,19 @@ export function Arrival() {
 
   const { id } = route.params as RouteParamProps;
 
+  const history = useObject(Historic, new BSON.UUID(id));
+
   return (
     <Container>
       <Header title="Chegada" />
       <Content>
         <Label>Placa do veículo</Label>
 
-        <LicensePlate>XXX0000</LicensePlate>
+        <LicensePlate>{history?.license_plate}</LicensePlate>
 
         <Label>Finalidade</Label>
 
-        <Description>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
-          libero esse velit iste, inventore iusto ab voluptatibus maiores quam
-          voluptatem beatae eaque est modi vel dolor! Fugiat est saepe illo?
-          Placeat sint quod beatae quibusdam, iusto ullam ut magnam, fugiat
-          velit porro vero itaque nihil. Perspiciatis tenetur, et inventore
-          sapiente iusto deserunt quidem veritatis at amet minima suscipit, cum
-          quae!
-        </Description>
+        <Description>{history?.description}</Description>
 
         <Footer>
           <ButtonIcon icon={X} />
