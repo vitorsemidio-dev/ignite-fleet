338291fd27c711b6c7dcbcbb2040fb5c62f3e6a9
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-17 01:39:02 -0300

    feat: cadastro saída do veículo

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-get-random-values';
 import {
   Roboto_400Regular,
   Roboto_700Bold,
--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native';
+import { useUser } from '@realm/react';
 import { useRef, useState } from 'react';
 import { Alert, ScrollView, TextInput } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -6,6 +8,8 @@ import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { LicensePlateInput } from '@components/LicensePlateInput';
 import { TextAreaInput } from '@components/TextInput';
+import { useRealm } from '@libs/realm';
+import { Historic } from '@libs/realm/schemas/Historic';
 import { licensePlateValidate } from '@utils/licensePlateValidate';
 
 import { Container, Content } from './styles';
@@ -13,6 +17,11 @@ import { Container, Content } from './styles';
 export function Departure() {
   const [description, setDescription] = useState('');
   const [licensePlate, setLicensePlate] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const realm = useRealm();
+  const user = useUser();
+  const { goBack } = useNavigation();
 
   const descriptionRef = useRef<TextInput>(null);
   const licensePlateRef = useRef<TextInput>(null);
@@ -34,9 +43,27 @@ export function Departure() {
           'Por favor, informe a finalidade da utilização do veículo',
         );
       }
+
+      setIsRegistering(false);
+
+      realm.write(() => {
+        realm.create(
+          'Historic',
+          Historic.generate({
+            user_id: user!.id,
+            license_plate: licensePlate,
+            description,
+          }),
+        );
+      });
+
+      Alert.alert('Saída', 'Saída do veículo registrada com sucesso.');
+
+      goBack();
     } catch (error) {
       console.log(error);
       Alert.alert('Erro', 'Não possível registrar a saída do veículo.');
+      setIsRegistering(false);
     }
   }
 
@@ -68,7 +95,11 @@ export function Departure() {
               onChangeText={setDescription}
             />
 
-            <Button title="Registar Saída" onPress={handleDepartureRegister} />
+            <Button
+              title="Registar Saída"
+              onPress={handleDepartureRegister}
+              isLoading={isRegistering}
+            />
           </Content>
         </ScrollView>
       </KeyboardAwareScrollView>
