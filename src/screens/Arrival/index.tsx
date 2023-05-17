831abfd1c9511c1e7ec8ba4343906812bee54531
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-05-17 02:01:42 -0300

    feat: criação layout tela de chegada

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,6 +1,17 @@
 import { useRoute } from '@react-navigation/native';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Description,
+  Footer,
+  Label,
+  LicensePlate,
+} from './styles';
+import { Header } from '@components/Header';
+import { Button } from '@components/Button';
+import { ButtonIcon } from '@components/ButtonIcon';
+import { X } from 'phosphor-react-native';
 
 type RouteParamProps = {
   id: string;
@@ -11,7 +22,31 @@ export function Arrival() {
 
   const { id } = route.params as RouteParamProps;
 
-  console.log(id);
+  return (
+    <Container>
+      <Header title="Chegada" />
+      <Content>
+        <Label>Placa do veículo</Label>
 
-  return <Container></Container>;
+        <LicensePlate>XXX0000</LicensePlate>
+
+        <Label>Finalidade</Label>
+
+        <Description>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem
+          libero esse velit iste, inventore iusto ab voluptatibus maiores quam
+          voluptatem beatae eaque est modi vel dolor! Fugiat est saepe illo?
+          Placeat sint quod beatae quibusdam, iusto ullam ut magnam, fugiat
+          velit porro vero itaque nihil. Perspiciatis tenetur, et inventore
+          sapiente iusto deserunt quidem veritatis at amet minima suscipit, cum
+          quae!
+        </Description>
+
+        <Footer>
+          <ButtonIcon icon={X} />
+          <Button title="Registrar chegada" />
+        </Footer>
+      </Content>
+    </Container>
+  );
 }
