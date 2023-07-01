19498f3f65f8b734634df21171bd448555cd72e3
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-07-01 06:54:11 -0300

    feat: adicionado google api key

---
+++ app.config.js
@@ -0,0 +1,52 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+module.exports = {
+  expo: {
+    name: 'ignitefleet',
+    slug: 'ignitefleet',
+    scheme: 'ignitefleet',
+    version: '1.0.0',
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'dark',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'cover',
+      backgroundColor: '#202024',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.vitorsemidiodev.ignitefleet',
+      config: {
+        googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#202024',
+      },
+      package: 'com.vitorsemidiodev.ignitefleet',
+      config: {
+        googleMaps: {
+          apiKey: process.env.GOOGLE_MAPS_API_KEY,
+        },
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+    plugins: [
+      [
+        'expo-location',
+        {
+          locationAlwaysAndWhenInUsePermission:
+            'Allow $(PRODUCT_NAME) to use your location.',
+        },
+      ],
+    ],
+  },
+};
