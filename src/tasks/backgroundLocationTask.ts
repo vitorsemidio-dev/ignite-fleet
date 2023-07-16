4d8bccc7ba62f9b04c3eff1cf1906ef7981b0a2b
Vitor Emidio <vitorsemidio.96@gmail.com>
2023-07-16 10:28:51 -0300

    feat: salvando dados de localização no dispositivo

--- src/tasks/backgroundLocationTask.ts
+++ src/tasks/backgroundLocationTask.ts
@@ -1,3 +1,4 @@
+import { saveStorageLocation } from '@libs/asyncStorage/locationStorage';
 import {
   Accuracy,
   hasStartedLocationUpdatesAsync,
@@ -8,23 +9,26 @@ import * as TaskManager from 'expo-task-manager';
 
 export const BACKGROUND_TASK_NAME = 'location-tracking';
 
-TaskManager.defineTask(BACKGROUND_TASK_NAME, ({ data, error }: any) => {
+TaskManager.defineTask(BACKGROUND_TASK_NAME, async ({ data, error }: any) => {
   try {
     if (error) {
       throw error;
     }
 
-    const { coords, timestamp } = data.locations[0];
+    if (data) {
+      const { coords, timestamp } = data.locations[0];
 
-    const currentLocation = {
-      latitude: coords.latitude,
-      longitude: coords.longitude,
-      timestamp,
-    };
+      const currentLocation = {
+        latitude: coords.latitude,
+        longitude: coords.longitude,
+        timestamp,
+      };
 
-    console.log(currentLocation);
+      await saveStorageLocation(currentLocation);
+    }
   } catch (error) {
     console.log(error);
+    await stopLocationTask();
   }
 });
 
